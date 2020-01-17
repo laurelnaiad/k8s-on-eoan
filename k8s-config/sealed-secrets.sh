f3180d3bfd93c5f52f2f249be6584986786140e2
Daphne Maddox <laurelnaiad@gmail.com>
2020-01-17 13:09:53 -0500

    fix: give sealed-secrets more time to create controller cert

--- k8s-config/sealed-secrets.sh
+++ k8s-config/sealed-secrets.sh
@@ -9,6 +9,6 @@ wget https://github.com/bitnami-labs/sealed-secrets/releases/download/v0.9.6/kub
     -O $MYDIR/kubeseal
 sudo install -m 755 $MYDIR/kubeseal /usr/local/bin/kubeseal
 kubectl apply -f https://github.com/bitnami-labs/sealed-secrets/releases/download/v0.9.6/controller.yaml
-sleep 7
+sleep 15
 kubeseal --fetch-cert --controller-namespace kube-system > $SSCERT
 # retry that last one if it fails the first (few) time(s)
