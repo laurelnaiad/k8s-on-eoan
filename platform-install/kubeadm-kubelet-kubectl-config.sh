c0cb82d9b3b5fb3b626992f70abd8fb414651b03
Daphne Maddox <laurelnaiad@gmail.com>
2020-01-17 13:36:54 -0500

    fix: taint master node

--- platform-install/kubeadm-kubelet-kubectl-config.sh
+++ platform-install/kubeadm-kubelet-kubectl-config.sh
@@ -104,3 +104,5 @@ rm -rf ~/.kube
 mkdir -p $HOME/.kube
 sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 sudo chown $(id -u):$(id -g) $HOME/.kube/config
+
+kubectl taint nodes --all node-role.kubernetes.io/master-
